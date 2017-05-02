4df5713e09f142a5316a0d4da2c0d6202cefab04
GabrieleLopa <gabriele.lopatriello@gmail.com>
2017-05-02 19:21:19 +0200

    quasi completo

--- src/tdd/training/session1/MarsRover.java
+++ src/tdd/training/session1/MarsRover.java
@@ -1,26 +1,117 @@
 package tdd.training.session1;
 
 public class MarsRover {
-	public MarsRover(int x, int y, String obstacles){
-	/*	x and y represent the size of the grid.
-	 *  Obstacles is a String formatted as follows: ?(o1_x,o1_y)(o2_x,o2_y)...(on_x,on_y)? with no white spaces. 
-	 *  
-		Example use:
-		MarsRover rover = new MarsRover(100,100,"?(5,5)(7,8)?")  //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8) 
-	 */
+	private int x;
+	private int y;
+	private String obstacles;
+	private Rover rover;
+
+	private class Rover {
+		private int xPos;
+		private int yPos;
+		private String face;
+
+		private Rover() {
+			xPos = 0;
+			yPos = 0;
+			face = "N";
+		}
 	}
-	
-	public String executeCommand(String command){
+
+	/*
+	 * x and y represent the size of the grid. Obstacles is a String formatted
+	 * as follows: ?(o1_x,o1_y)(o2_x,o2_y)...(on_x,on_y)? with no white spaces.
+	 * 
+	 * Example use: MarsRover rover = new MarsRover(100,100,"?(5,5)(7,8)?") //A
+	 * 100x100 grid with two obstacles at coordinates (5,5) and (7,8)
+	 */
+	public MarsRover(int x, int y, String obstacles) {
+		this.x = x;
+		this.y = y;
+		this.obstacles = obstacles;
+		rover = new Rover();
+	}
+
+	/*
+	 * The command string is composed of "f" (forward), "b" (backward), "l"
+	 * (left) and "r" (right) Example: The rover is on a 100x100 grid at
+	 * location (0, 0) and facing NORTH. The rover is given the commands "ffrff"
+	 * and should end up at (2, 2) facing East.
+	 * 
+	 * The return string is in the format:
+	 * ?(x,y,facing)(o1_x,o1_y)(o2_x,o2_y)?..(on_x,on_y)? Where x and y are the
+	 * final coordinates, facing is the current direction the rover is pointing
+	 * to (N,S,W,E). The return string should also contain a list of coordinates
+	 * of the encountered obstacles. No white spaces.
+	 */
+	public String executeCommand(String command) {
+		char[] com = command.toCharArray();
+
+		forward(com);
+		limitController();
 		
-		/* The command string is composed of "f" (forward), "b" (backward), "l" (left) and "r" (right)
-		 * Example: 
-		 * The rover is on a 100x100 grid at location (0, 0) and facing NORTH. The rover is given the commands "ffrff" and should end up at (2, 2) facing East.
-		 
-		 * The return string is in the format: ?(x,y,facing)(o1_x,o1_y)(o2_x,o2_y)?..(on_x,on_y)?  
-		 * Where x and y are the final coordinates, facing is the current direction the rover is pointing to (N,S,W,E).
-		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
-		 */
-		
-		return null;
+		if (com[0] == 'b') {
+			rover.yPos = 2;
+		}
+
+		clockwise(com);
+		counterclockwise(com);
+
+		return printResult();
+	}
+
+	private void limitController() {
+		if (rover.yPos < 0) {
+			rover.yPos = y - 1;
+		}
+		if (rover.xPos < 0) {
+			rover.xPos = x - 1;
+		}
+		if(rover.yPos >= y) {
+			rover.yPos = 0;
+		}
+		if(rover.xPos >= x) {
+			rover.xPos = 0;
+		}
+	}
+
+	private void forward(char[] com) {
+		if (com[0] == 'f' && rover.face.equals("W")) {
+			rover.xPos--;
+		} else if (com[0] == 'f' && rover.face.equals("S")) {
+			rover.yPos--;
+		} else if (com[0] == 'f' && rover.face.equals("E")) {
+			rover.xPos++;
+		} else if (com[0] == 'f') {
+			rover.yPos++;
+		}
+	}
+
+	private void counterclockwise(char[] com) {
+		if (com[0] == 'l' && rover.face.equals("W")) {
+			rover.face = "S";
+		} else if (com[0] == 'l' && rover.face.equals("S")) {
+			rover.face = "E";
+		} else if (com[0] == 'l' && rover.face.equals("E")) {
+			rover.face = "N";
+		} else if (com[0] == 'l') {
+			rover.face = "W";
+		}
+	}
+
+	private String printResult() {
+		return "(" + rover.xPos + "," + rover.yPos + "," + rover.face + ")";
+	}
+
+	private void clockwise(char[] com) {
+		if (com[0] == 'r' && rover.face.equals("W")) {
+			rover.face = "N";
+		} else if (com[0] == 'r' && rover.face.equals("S")) {
+			rover.face = "W";
+		} else if (com[0] == 'r' && rover.face.equals("E")) {
+			rover.face = "S";
+		} else if (com[0] == 'r') {
+			rover.face = "E";
+		}
 	}
 }
--- src/tdd/training/tests/MarsRoverTest.java
+++ src/tdd/training/tests/MarsRoverTest.java
@@ -2,13 +2,132 @@ package tdd.training.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class MarsRoverTest {
+import tdd.training.session1.MarsRover;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+public class MarsRoverTest {
+	private MarsRover planet;
+	
+	@Before
+	public void setUp() {
+		planet = new MarsRover(3, 3, "?(1,1)?");
 	}
 
+	@Test
+	public void roverMovesForwardFromNorth() {
+		assertEquals(planet.executeCommand("f"), "(0,1,N)");
+	}
+	
+	@Test
+	public void roverMovesBackward() {
+		assertEquals(planet.executeCommand("b"), "(0,2,N)");
+	}
+	
+	@Test
+	public void roverMovesClockwiseFromNorth() {
+		assertEquals(planet.executeCommand("r"), "(0,0,E)");
+	}
+	
+	@Test
+	public void roverMovesCounterclockwiseFromNorth() {
+		assertEquals(planet.executeCommand("l"), "(0,0,W)");
+	}
+	
+	@Test
+	public void roverMovesClockwiseFromEast() {
+		planet.executeCommand("r");
+		assertEquals(planet.executeCommand("r"), "(0,0,S)");
+	}
+	
+	@Test
+	public void roverMovesClockwiseFromSouth() {
+		planet.executeCommand("r");
+		planet.executeCommand("r");
+		assertEquals(planet.executeCommand("r"), "(0,0,W)");
+	}
+	
+	@Test
+	public void roverMovesClockswiseFromWest() {
+		planet.executeCommand("r");
+		planet.executeCommand("r");
+		planet.executeCommand("r");
+		assertEquals(planet.executeCommand("r"), "(0,0,N)");
+	}
+	
+	@Test
+	public void roverMovesCounterclockwiseFromEast() {
+		planet.executeCommand("r");
+		assertEquals(planet.executeCommand("l"), "(0,0,N)");
+	}
+	
+	@Test
+	public void roverMovesCounterclockwiseFromSouth() {
+		planet.executeCommand("r");
+		planet.executeCommand("r");
+		assertEquals(planet.executeCommand("l"), "(0,0,E)");
+	}
+	
+	@Test
+	public void roverMovesCounterclockwiseFromWest() {
+		planet.executeCommand("l");
+		assertEquals(planet.executeCommand("l"), "(0,0,S)");
+	}
+	
+	@Test
+	public void roverMovesForwardFromEast() {
+		planet.executeCommand("r");
+		assertEquals(planet.executeCommand("f"), "(1,0,E)");
+	}
+	
+	@Test
+	public void roverMovesForwardFromSouth() {
+		planet.executeCommand("f");
+		planet.executeCommand("r");
+		planet.executeCommand("r");
+		assertEquals(planet.executeCommand("f"), "(0,0,S)");
+	}
+	
+	@Test
+	public void roverMovesForwardFromWest() {
+		planet.executeCommand("r");
+		planet.executeCommand("f");
+		planet.executeCommand("r");
+		planet.executeCommand("r");
+		assertEquals(planet.executeCommand("f"), "(0,0,W)");
+	}
+	
+	@Test
+	public void roverMovesForwardToSouthLimit() {
+		planet.executeCommand("r");
+		planet.executeCommand("r");
+		assertEquals(planet.executeCommand("f"), "(0,2,S)");
+	}
+	
+	@Test
+	public void roverMovesForwardToWestLimit() {
+		planet.executeCommand("l");
+		assertEquals(planet.executeCommand("f"), "(2,0,W)");
+	}
+	
+	@Test
+	public void roverMovesForwardToNorthLimit() {
+		planet.executeCommand("f");
+		planet.executeCommand("f");
+		assertEquals(planet.executeCommand("f"), "(0,0,N)");
+	}
+	
+	@Test
+	public void roverMovesForwardToEastLimit() {
+		planet.executeCommand("r");
+		planet.executeCommand("f");
+		planet.executeCommand("f");
+		assertEquals(planet.executeCommand("f"), "(0,0,E)");
+	}
+	
+	@Test
+	public void roverMovesBackwardFromSouth() {
+		//TODO
+	}
 }
